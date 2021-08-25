a6e4e7c92867ba989d69bd87a5ea70caf19d6581
mollima <jaike92@gmail.com>
2021-08-25 13:51:23 +0300

    Just trying to practice

--- Patrick/Append.py
+++
@@ -1,4 +0,0 @@
-lucky_numbers = [4, 7, 23, 25, 36]
-friends = ["Mia", "July_wife", "July_work", "Phill", "Dima"]
-friends.append("Dick")  # Функция Аппед, добовлет нужные данные в масив
-print(friends)
